78524114d6aa68cc39fd2e096bd9258769a6443e
janpawel212121-droid <janpawel212121@gmail.com>
2026-05-08 15:56:16 +0200

    Add Supabase production config

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.HEARTFARM_CONFIG = {
+    supabaseUrl: "https://bgurqdtklagvqznvphdy.supabase.co",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJndXJxZHRrbGFndnF6bnZwaGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMzk0OTQsImV4cCI6MjA5MzgxNTQ5NH0.ZTmToLhISPBaCuiyrjJc9qvlo0uUAxaHWDazh5ktcPg",
+    pollMs: 15000,
+};
